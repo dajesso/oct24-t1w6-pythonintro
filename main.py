6ea67057e8e1f67b1e87d50d99e0e95c82d3b4c6
Jessica Amy <jessicaannetteamy@gmail.com>
2024-11-18 20:28:43 +1030

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+# print(2 + 3)
+
+# # The bellow line prints hello world
+# # Python interper will ignore comments
+
+# print("hello felicis!!")
+
+# print("Goodbye!") # See ya felicis!
+
+# Data Types
+
+# Numbers
+
+# int
+# 5
+# 1000
+
+
+
+# logical operators
+# and, or, not
+
+print(True and False)
+print(False or True)
+print(5 > 3 and 2 < 5)
+
+# Variables
+
+num1 = 5
+num2 = 4
+
+print(num1 + num2)
+num2 = 20
+print(num1 + num2)
+
+# String interpolation
+
+name = "Jesso"
+print("Hello wonderful " + name + " nice to meet you")
+# f-string
+
+print(f"Hello wonderful {name} nice to meet you")
+
+
+print(f"Hello wonderful {4 * 5} nice to meet you")
